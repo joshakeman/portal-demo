cd1575ed79da9b92bf03ee9577ba682347622eba
Josh Akeman <joshakeman@gmail.com>
2020-07-08 08:23:41 -0400

    got web server running

---
+++ app/data-jobs-api/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	"expvar"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"portal-demo/app/data-jobs-api/handlers"
+	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
+)
+
+// build is the git version of this program. It is set using build flags in the makefile.
+var build = "develop"
+
+func main() {
+	log := log.New(os.Stdout, "METADATA : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	if err := run(log); err != nil {
+		log.Println("main: error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *log.Logger) error {
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+		DB struct {
+			User       string `conf:"default:mysql"`
+			Password   string `conf:"default:mysql,noprint"`
+			Host       string `conf:"default:db"`
+			Name       string `conf:"default:mysql"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+	}
+	cfg.Version.SVN = build
+	cfg.Version.Desc = "copyright information here"
+
+	if err := conf.Parse(os.Args[1:], "METADATA", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("METADATA", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("METADATA", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
+	// =========================================================================
+	// App Starting
+
+	// Print the build version for our logs. Also expose it under /debug/vars.
+	expvar.NewString("build").Set(build)
+	log.Printf("main : Started : Application initializing : version %q", build)
+	defer log.Println("main: Completed")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main: Config :\n%v\n", out)
+
+	// =========================================================================
+	// Start API Service
+
+	log.Println("main: Initializing API support")
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	api := http.Server{
+		Addr: cfg.Web.APIHost,
+		// Handler:      handlers.API(build, shutdown, log, db, a),
+		Handler:      handlers.API(build, shutdown),
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for requests.
+	go func() {
+		log.Printf("main: API listening on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// =========================================================================
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
+	select {
+	case err := <-serverErrors:
+		return errors.Wrap(err, "server error")
+
+	case sig := <-shutdown:
+		log.Printf("main: %v : Start shutdown", sig)
+
+		// Give outstanding requests a deadline for completion.
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		// Asking listener to shutdown and shed load.
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return errors.Wrap(err, "could not stop server gracefully")
+		}
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module portal-demo
+
+go 1.14
+
+require (
+	github.com/ardanlabs/conf v1.3.2
+	github.com/dimfeld/httptreemux/v5 v5.2.2
+	github.com/google/go-cmp v0.5.0 // indirect
+	github.com/pkg/errors v0.9.1
+)
